8c7a321d9d229d5d843846a780c37e05c1840bbd
temics <88716615+temics@users.noreply.github.com>
2023-06-20 19:07:08 +0500

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const navbar = document.querySelector(".navbar");
+const menuToggle = document.querySelector(".menu-toggle");
+menuToggle.addEventListener("click", () => {
+  navbar.classList.toggle("open");
+});
